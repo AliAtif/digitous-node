1e1890d2da420328deec128565fd8e6fc04860e5
atif <at07.if@gmail.com>
2021-03-01 18:39:24 +0100

    chapitre1

---
+++ chapitre_01/index.js
@@ -0,0 +1,66 @@
+const { request, response } = require('express');
+const express = require('express');
+const app = express();
+
+const port = 8000;
+app.listen(port, () => {
+  console.log('Server started on port: ' + port);
+});
+
+app.get("/", (request, response) => {
+    response.send("Authors API");
+})
+
+
+/*Exercice 1 */
+
+
+const authors = ["Lawrence Nowell, UK", "William Shakespeare, UK",
+ "Charles Dickens, US", "Oscar Wilde, UK"]
+ 
+const books = ["Beowulf", "Hamlet, Othello, Romeo and Juliet, MacBeth", 
+"Oliver Twist, A Christmas Carol", "The Picture of Dorian Gray, The Importance of Being Earnest"]
+
+const data = [{
+    id : 1,
+    author: "Lawrence Nowell",
+    nationality: "UK",
+    book: "Beowulf"
+},
+
+ {
+    id : 2,
+    author: "William Shakespeare",
+    nationality: "UK",
+    book: "Hamlet, Othello, Romeo and Juliet, MacBeth"
+},
+
+ {
+    id : 3,
+    author: "Charles Dickens",
+    nationality: "US",
+    book: "Oliver Twist, A Christmas Carol"
+}, 
+
+{   
+    id : 4,
+    author: "Oscar Wilde",
+    nationality: "UK",
+    book: "The Picture of Dorian Gray, The Importance of Being Earnest",
+}]
+
+app.get("/authors/:id", (request, response) => {
+    if (authors[request.params.id] === undefined) {
+        response.send(`The author with the ID ${request.params.id} does not exist`)
+    }
+     else{
+        response.send(`${authors[request.params.id]}`)
+     }}
+
+     )
+      
+
+app.get("/cars/", (request, res) => {
+    response.send("error")
+})
+
