fd5c674a4a0ee50c131e3603584ff94173bf8d99
atif <at07.if@gmail.com>
2021-03-03 15:28:12 +0100

    formulaire avec correction

--- formulaire/index.js
+++ formulaire/index.js
@@ -1,34 +1,45 @@
-
 const express = require("express");
-const expressHandlebars = require("express-handlebars")
+const expressHandlebars = require("express-handlebars");
 
 const app = express();
 const port = 8000;
-let logged = false
 
-app.engine('handlebars', expressHandlebars());
+let userConnected = false;
+
+app.engine("handlebars", expressHandlebars());
 app.set("view engine", "handlebars");
-app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Serveur lancé ${port}`)
-})
+  console.log("Serveur lancé");
+});
 
 app.get("/", (req, res) => {
-    res.render("home")
-})
+  res.render("login", {passwordTooShort: false});
+});
 
-app.use(express.urlencoded({ extended: true }))
+app.get("/home", (req, res) => {
+  if (userConnected) {
+    res.render("home", {
+        title: "Bonjour",
+        sousTitre: "Ca va ? "
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/login", (req, res) => {
+  res.send("Yeshi's route !");
+});
+
 app.post("/login", (req, res) => {
-    console.log(req.body)
-    logged = true;
-    res.redirect("/success")
-})
-
-app.get("/success", (req, res) => {
-    if (logged) {
-        res.send("Login successfully")
-    } else {
-        res.redirect("/")
-    }
-})
+  console.log(req.body);
+  if (req.body.password.length < 8) {
+    res.render("login", {passwordTooShort: true});
+  } else {
+    userConnected = true;
+    res.redirect("/home");
+  }
+});
